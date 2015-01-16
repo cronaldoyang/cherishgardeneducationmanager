83f2f27aa28a7b82e695471ac06dc5680ac15b10
xiaoyang <253935659@qq.com>
2015-01-16 23:44:54 +0800

    Let the username textbox and passowrdbox give the hint text if they are keyboard focused

--- CherishGardenEcucationManager/App.xaml.cs
+++ CherishGardenEcucationManager/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace CherishGardenEcucationManager
+namespace CherishGardenEducationManager
 {
     /// <summary>
     /// Interaction logic for App.xaml
