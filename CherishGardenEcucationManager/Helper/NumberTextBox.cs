fb29d80bda9c241142371b24e22f6911077ec91c
cronaldoyang <cronaldo.yang@gmail.com>
2015-01-29 12:19:43 +0800

    Finish the textbox's drop down listitem for gender and putonghualevel

---
+++ CherishGardenEcucationManager/Helper/NumberTextBox.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CherishGardenEducationManager
+{
+    public class NumberTextBox : TextBox
+    {
+        #region Constructors
+            
+        public NumberTextBox()
+        {
+            TextChanged += new TextChangedEventHandler(OnTextChanged);
+            KeyDown += new KeyEventHandler(OnKeyDown);
+        }
+        #endregion
+
+        #region Properties
+        new public String Text
+        {
+            get { return base.Text; }
+            set
+            {
+                base.Text = LeaveOnlyNumbers(value);
+            }
+        }
+
+        #endregion
+
+        #region Functions
+        private bool IsNumberKey(Key inKey)
+        {
+            if (inKey < Key.D0 || inKey > Key.D9)
+            {
+                if (inKey < Key.NumPad0 || inKey > Key.NumPad9)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsDelOrBackspaceOrTabKey(Key inKey)
+        {
+            return inKey == Key.Delete || inKey == Key.Back || inKey == Key.Tab;
+        }
+
+        private string LeaveOnlyNumbers(String inString)
+        {
+            String tmp = inString;
+            foreach (char c in inString.ToCharArray())
+            {
+                if (!System.Text.RegularExpressions.Regex.IsMatch(c.ToString(), "^[0-9]*$"))
+                {
+                    tmp = tmp.Replace(c.ToString(), "");
+                }
+            }
+            return tmp;
+        }
+        #endregion
+
+        #region Event Functions
+        protected void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = !IsNumberKey(e.Key) && !IsDelOrBackspaceOrTabKey(e.Key);
+        }
+
+        protected void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            base.Text = LeaveOnlyNumbers(Text);
+        }
+        #endregion
+    }
+}
--- CherishGardenEcucationManager/TeacherInfoPage.xaml.cs
+++ CherishGardenEcucationManager/TeacherInfoPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,5 +24,7 @@ namespace CherishGardenEducationManager
         {
             InitializeComponent();
         }
+
+
     }
 }
