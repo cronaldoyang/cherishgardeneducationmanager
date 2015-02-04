448b19b45b3a63781422576f9fd3fd410c52990b
cronaldoyang <cronaldo.yang@gmail.com>
2015-02-05 00:31:06 +0800

    convert from  photopath to photo.

---
+++ CherishGardenEcucationManager/Converter/TypeToDisplayPhotoFromPath.cs
@@ -0,0 +1,25 @@
+﻿using CherishGardenEducationManager.Entity;
+using CherishGardenEducationManager.Helper;
+using CherishGardenEducationManager.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace CherishGardenEducationManager
+{
+    class TypeToDisplayPhotoFromPath : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string   photopath = (string)value;
+            return ChoosePhotoHelper.getRealPhoto(photopath);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
