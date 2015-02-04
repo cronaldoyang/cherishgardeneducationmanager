b3bd57ea25233a143453b2f4530ff18548f9de12
cronaldoyang <cronaldo.yang@gmail.com>
2015-02-04 16:03:22 +0800

    Added TypeToDisplayGradeNameConvert to implement get grade name from gradeid.

---
+++ CherishGardenEcucationManager/Converter/TypeToDisplayGradeNameConverter.cs
@@ -0,0 +1,24 @@
+﻿using CherishGardenEducationManager.Entity;
+using CherishGardenEducationManager.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace CherishGardenEducationManager
+{
+    class TypeToDisplayGradeNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int  gradeid = (int)value;
+            return ClassViewModel.getInstance().getGradeNameByid(gradeid);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
